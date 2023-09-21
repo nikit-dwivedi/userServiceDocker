0755bde1b61521e82ff5a7291ef49bbbe17901c1
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-09-22 02:00:21 +0530

    bug fix

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 require('dotenv').config()
 process.env.TZ = 'Asia/Calcutta';
-const port = process.env.PORT || 4006;
+const port = process.env.APPID;
 const IP = process.env.IP || '127.0.0.1';
 const server = http.createServer(app);
 server.listen(port, () => {
